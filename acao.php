04e8153a518a22c1b54ab79fc152a6f038634fe7
etecjga <midori.kato@etec.sp.gov.br>
2025-08-13 13:49:17 -0300

    adicionando o link ancora a cada card de jogo

--- acao.php
+++ acao.php
@@ -29,35 +29,48 @@ if (!isset($_SESSION['user_id'])) {
     </header>
 
     <section class="games">
-        <div class="card">
-            <img src="images/fortnite.png" alt="" class="thumbnails">
-            <p class="description">Fortnite é um jogo eletrônico multijogador online revelado originalmente em 2011, desenvolvido pela Epic Games e lançado como diferentes modos de jogo que compartilham a mesma jogabilidade e motor gráfico de jogo.</p>
-        </div>
+    <div class="card">
+    <a href="https://www.fortnite.com/" target="_blank">
+        <img src="images/fortnite.png" alt="Fortnite" class="thumbnails">
+    </a>
+    <p class="description">Fortnite é um jogo eletrônico multijogador online revelado originalmente em 2011, desenvolvido pela Epic Games e lançado como diferentes modos de jogo que compartilham a mesma jogabilidade e motor gráfico de jogo.</p>
+</div>
 
-        <div class="card">
-            <img src="images/csgo.png" alt="" class="thumbnails">
-            <p class="description">Counter-Strike: Global Offensive (CS:GO) foi um jogo online desenvolvido pela Valve Corporation e pela Hidden Path Entertainment, sendo uma sequência de Counter-Strike: Source. É o quarto título principal da franquia.</p>
-        </div>
+<div class="card">
+    <a href="https://www.counter-strike.net/cs2" target="_blank">
+        <img src="images/csgo.png" alt="CS:GO" class="thumbnails">
+    </a>
+    <p class="description">Counter-Strike: Global Offensive (CS:GO) foi um jogo online desenvolvido pela Valve Corporation e pela Hidden Path Entertainment, sendo uma sequência de Counter-Strike: Source. É o quarto título principal da franquia.</p>
+</div>
 
-        <div class="card">
-            <img src="images/callofduty.png" alt="" class="thumbnails">
-            <p class="description">Call of Duty é uma série de jogos eletrônicos de tiro em primeira pessoa. O primeiro título da série foi lançado em 2003 exclusivamente para computadores. Mais tarde a série se expandiu para os mais variados sistemas, consoles, portáteis e smartphones.</p>
-        </div>
+<div class="card">
+    <a href="https://www.callofduty.com/" target="_blank">
+        <img src="images/callofduty.png" alt="Call of Duty" class="thumbnails">
+    </a>
+    <p class="description">Call of Duty é uma série de jogos eletrônicos de tiro em primeira pessoa. O primeiro título da série foi lançado em 2003 exclusivamente para computadores. Mais tarde a série se expandiu para os mais variados sistemas, consoles, portáteis e smartphones. </p>
+</div>
 
-        <div class="card">
-            <img src="images/mortalkombat.png" alt="" class="thumbnails">
-            <p class="description">Mortal Kombat é uma franquia americana de jogos eletrônicos de luta originalmente desenvolvidos e publicados pela Midway Games com seu primeiro lançamento em 1992 para arcades.</p>
-        </div>
+<div class="card">
+    <a href="https://www.mortalkombat.com/" target="_blank">
+        <img src="images/mortalkombat.png" alt="Mortal Kombat" class="thumbnails">
+    </a>
+    <p class="description">Mortal Kombat é uma franquia americana de jogos eletrônicos de luta originalmente desenvolvidos e publicados pela Midway Games com seu primeiro lançamento em 1992 para arcades. Em 2011, depois da falência da Midway Games, a produção de Mortal Kombat foi adquirida pela Warner Bros. </p>
+</div>
 
-        <div class="card">
-            <img src="images/brawlhalla.png" alt="" class="thumbnails">
-            <p class="description">Brawlhalla é um jogo de luta de plataforma gratuito desenvolvido pela Blue Mammoth Games. Foi lançado originalmente para macOS, PlayStation 4 e Windows em 2017, com versões para Nintendo Switch, Xbox One, Android e iOS lançadas posteriormente. </p>
-        </div>
+<div class="card">
+    <a href="https://www.brawlhalla.com/" target="_blank">
+        <img src="images/brawlhalla.png" alt="Brawlhalla" class="thumbnails">
+    </a>
+    <p class="description">Brawlhalla é um jogo de luta de plataforma gratuito desenvolvido pela Blue Mammoth Games. Foi lançado originalmente para macOS, PlayStation 4 e Windows em 2017, com versões para Nintendo Switch, Xbox One, Android e iOS lançadas posteriormente. </p>
+</div>
 
-        <div class="card">
-            <img src="images/assassinscreed.png" alt="" class="thumbnails">
-            <p class="description">Assassin's Creed é uma série de jogos eletrônicos do gênero ação-aventura e RPG desenvolvida e publicada pela empresa Ubisoft.</p>
-        </div>
+<div class="card">
+    <a href="https://www.ubisoft.com/game/assassins-creed" target="_blank">
+        <img src="images/assassinscreed.png" alt="Assassin's Creed" class="thumbnails">
+    </a>
+    <p class="description">Assassin's Creed é uma série de jogos eletrônicos do gênero ação-aventura e RPG desenvolvida e publicada pela empresa Ubisoft.</p>
+</div>
+
     </section>
 
     <footer>
