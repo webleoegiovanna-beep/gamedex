6b23d3f65a21078ae58cc3bb4bc745e220a11faa
etecjga <midori.kato@etec.sp.gov.br>
2025-08-13 13:54:01 -0300

    adicionando o ancora do game a cada card

--- estrategia.php
+++ estrategia.php
@@ -29,36 +29,48 @@ if (!isset($_SESSION['user_id'])) {
     </header>
 
     <section class="games">
-        <div class="card">
-            <img src="images/clashroyale.png" alt="" class="thumbnails">
-            <p class="description">Clash Royale é um videojogo de estratégia freemium desenvolvido e publicado pela Supercell, empresa sediada em Helsinki, na Finlândia.</p>
-        </div>
+    <div class="card">
+        <a href="https://clashroyale.com/" target="_blank">
+            <img src="images/clashroyale.png" alt="Clash Royale" class="thumbnails">
+        </a>
+        <p class="description">Clash Royale é um videojogo de estratégia freemium desenvolvido e publicado pela Supercell, empresa sediada em Helsinki, na Finlândia.</p>
+    </div>
 
-        <div class="card">
-            <img src="images/PvZ.png" alt="" class="thumbnails">
-            <p class="description">Plants vs. Zombies é um jogo eletrônico de tower defense e estratégia desenvolvido e originalmente publicado pela PopCap Games para Windows e OS X em 5 de maio de 2009. Desde então, foi portado para consoles, portáteis, e dispositivos móveis.</p>
-        </div>
+    <div class="card">
+        <a href="https://www.ea.com/games/plants-vs-zombies" target="_blank">
+            <img src="images/PvZ.png" alt="Plants vs Zombies" class="thumbnails">
+        </a>
+        <p class="description">Plants vs. Zombies é um jogo eletrônico de tower defense e estratégia desenvolvido e originalmente publicado pela PopCap Games para Windows e OS X em 5 de maio de 2009. Desde então, foi portado para consoles, portáteis, e dispositivos móveis.</p>
+    </div>
 
-        <div class="card">
-            <img src="images/LoL.png" alt="" class="thumbnails">
-            <p class="description">League of Legends (abreviado como LoL e comumente referido como League) é um jogo eletrônico do gênero multiplayer online battle arena (MOBA) desenvolvido e publicado pela Riot Games.</p>
-        </div>
+    <div class="card">
+        <a href="https://www.leagueoflegends.com/" target="_blank">
+            <img src="images/LoL.png" alt="League of Legends" class="thumbnails">
+        </a>
+        <p class="description">League of Legends (abreviado como LoL e comumente referido como League) é um jogo eletrônico do gênero multiplayer online battle arena (MOBA) desenvolvido e publicado pela Riot Games.</p>
+    </div>
 
-        <div class="card">
-            <img src="images/brawlStars.png" alt="" class="thumbnails">
-            <p class="description">Brawl Stars é um jogo eletrônico mobile focado nos estilos de arena de batalha, multijogador online e habilidades em terceira pessoa, desenvolvido e publicado pela empresa produtora de jogos Supercell. </p>
-        </div>
+    <div class="card">
+        <a href="https://supercell.com/en/games/brawlstars/" target="_blank">
+            <img src="images/brawlStars.png" alt="Brawl Stars" class="thumbnails">
+        </a>
+        <p class="description">Brawl Stars é um jogo eletrônico mobile focado nos estilos de arena de batalha, multijogador online e habilidades em terceira pessoa, desenvolvido e publicado pela empresa produtora de jogos Supercell. </p>
+    </div>
 
-        <div class="card">
-            <img src="images/ClashofClans.png" alt="" class="thumbnails">
-            <p class="description">Clash of Clans é um jogo de estratégia desenvolvido pela Supercell, lançado em 2012 para iOS e no ano seguinte para Android. </p>
-        </div>
+    <div class="card">
+        <a href="https://supercell.com/en/games/clashofclans/" target="_blank">
+            <img src="images/ClashofClans.png" alt="Clash of Clans" class="thumbnails">
+        </a>
+        <p class="description">Clash of Clans é um jogo de estratégia desenvolvido pela Supercell, lançado em 2012 para iOS e no ano seguinte para Android. </p>
+    </div>
 
-        <div class="card">
-            <img src="images/Plague.png" alt="" class="thumbnails">
-            <p class="description">Plague Inc: Evolved é um jogo de estratégia e simulação para Windows, distribuído pelo Steam, também disponível para Mac OS X e Linux. No jogo, o jogador cria e desenvolve um agente patogênico com o objetivo de destruir toda a humanidade em uma epidemia global.</p>
-        </div>
-    </section>
+    <div class="card">
+        <a href="https://www.ndemiccreations.com/en/22-plague-inc" target="_blank">
+            <img src="images/Plague.png" alt="Plague Inc" class="thumbnails">
+        </a>
+        <p class="description">Plague Inc: Evolved é um jogo de estratégia e simulação para Windows, distribuído pelo Steam, também disponível para Mac OS X e Linux. No jogo, o jogador cria e desenvolve um agente patogênico com o objetivo de destruir toda a humanidade em uma epidemia global.</p>
+    </div>
+</section>
 
     <footer>
         <p>By Giovanna Camargo & Leonardo Weber</p>
--- terror.php
+++ terror.php
@@ -29,36 +29,49 @@ if (!isset($_SESSION['user_id'])) {
     </header>
 
     <section class="games">
-        <div class="card">
-            <img src="images/outlast.png" alt="" class="thumbnails">
-            <p class="description">Outlast é um jogo de survival horror desenvolvido e publicado pela Red Barrels, uma empresa fundada por pessoas previamente envolvidas com alguns jogos eletrônicos, tais como Prince of Persia, Assassin Creed, Tom Clancy's Splinter Cell e Uncharted.  </p>
-        </div>
+    <div class="card">
+        <a href="https://redbarrelsgames.com/games/outlast/" target="_blank">
+            <img src="images/outlast.png" alt="Outlast" class="thumbnails">
+        </a>
+        <p class="description">Outlast é um jogo de survival horror desenvolvido e publicado pela Red Barrels, uma empresa fundada por pessoas previamente envolvidas com alguns jogos eletrônicos, tais como Prince of Persia, Assassin Creed, Tom Clancy's Splinter Cell e Uncharted.  </p>
+    </div>
 
-        <div class="card">
-            <img src="images/five.png" alt="" class="thumbnails">
-            <p class="description">Five Nights at Freddy's é um jogo eletrônico independente de survival horror desenvolvido e publicado por Scott Cawthon. </p>
-        </div>
+    <div class="card">
+        <a href="https://store.steampowered.com/app/319510/Five_Nights_at_Freddys/" target="_blank">
+            <img src="images/five.png" alt="Five Nights at Freddy's" class="thumbnails">
+        </a>
+        <p class="description">Five Nights at Freddy's é um jogo eletrônico independente de survival horror desenvolvido e publicado por Scott Cawthon. </p>
+    </div>
 
-        <div class="card">
-            <img src="images/silent.png" alt="" class="thumbnails">
-            <p class="description">Silent Hill é uma série de jogos eletrônicos de survival horror e de tiro em terceira pessoa, desenvolvida e publicada pela Konami. </p>
-        </div>
+    <div class="card">
+        <a href="https://www.konami.com/games/silenthill/" target="_blank">
+            <img src="images/silent.png" alt="Silent Hill" class="thumbnails">
+        </a>
+        <p class="description">Silent Hill é uma série de jogos eletrônicos de survival horror e de tiro em terceira pessoa, desenvolvida e publicada pela Konami. </p>
+    </div>
 
-        <div class="card">
-            <img src="images/resident.png" class="thumbnails">
-            <p class="description">Resident Evil 7: Biohazard, conhecido no Japão como Biohazard 7: Resident Evil é um jogo eletrônico do gênero survival horror produzido pela Capcom e lançado em 24 de janeiro de 2017 para Microsoft Windows, PlayStation 4 e Xbox One, com a versão de PlayStation 4 tendo suporte completo para PlayStation VR.</p>
-        </div>
+    <div class="card">
+        <a href="https://www.residentevil.com/7/" target="_blank">
+            <img src="images/resident.png" alt="Resident Evil 7" class="thumbnails">
+        </a>
+        <p class="description">Resident Evil 7: Biohazard, conhecido no Japão como Biohazard 7: Resident Evil é um jogo eletrônico do gênero survival horror produzido pela Capcom e lançado em 24 de janeiro de 2017 para Microsoft Windows, PlayStation 4 e Xbox One, com a versão de PlayStation 4 tendo suporte completo para PlayStation VR.</p>
+    </div>
 
-        <div class="card">
-            <img src="images/phasmo.png" alt="" class="thumbnails">
-            <p class="description">Phasmophobia é um jogo de terror de sobrevivência independente desenvolvido e publicado pela Kinetic Games. O jogo foi disponibilizado para acesso antecipado por meio da Steam para Microsoft Windows em setembro de 2020, junto com suporte para realidade virtual.</p>
-        </div>
+    <div class="card">
+        <a href="https://store.steampowered.com/app/739630/Phasmophobia/" target="_blank">
+            <img src="images/phasmo.png" alt="Phasmophobia" class="thumbnails">
+        </a>
+        <p class="description">Phasmophobia é um jogo de terror de sobrevivência independente desenvolvido e publicado pela Kinetic Games. O jogo foi disponibilizado para acesso antecipado por meio da Steam para Microsoft Windows em setembro de 2020, junto com suporte para realidade virtual.</p>
+    </div>
 
-        <div class="card">
-            <img src="images/stay.png" alt="" class="thumbnails">
-            <p class="description">Stay out of the House é um simulador realista de fuga de assassinos em série. Você foi sequestrado e mantido em cativeiro na casa de um açougueiro canibal. E é apenas uma questão de tempo até que você vire o jantar. Fuja da maneira que puder.</p>
-        </div>
-    </section>
+    <div class="card">
+        <a href="https://store.steampowered.com/app/1543470/Stay_Out_of_the_House/" target="_blank">
+            <img src="images/stay.png" alt="Stay Out of the House" class="thumbnails">
+        </a>
+        <p class="description">Stay out of the House é um simulador realista de fuga de assassinos em série. Você foi sequestrado e mantido em cativeiro na casa de um açougueiro canibal. E é apenas uma questão de tempo até que você vire o jantar. Fuja da maneira que puder.</p>
+    </div>
+</section>
+
 
     <footer>
         <p>By Giovanna Camargo & Leonardo Weber</p>
