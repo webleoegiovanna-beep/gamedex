70d06bb656f1aba8805fe36006202139a5702c0a
etecjga <midori.kato@etec.sp.gov.br>
2025-08-13 13:15:29 -0300

    removendo informações desnecessárias

--- cadastro.php
+++ cadastro.php
@@ -1,37 +1,29 @@
 <?php
-// Conectar ao banco de dados
 $servername = "localhost";
-$username = "root"; // seu usuário do MySQL
-$password = ""; // sua senha do MySQL
-$dbname = "cadUsuario"; // nome do banco de dados
+$username = "root";
+$password = ""; 
+$dbname = "cadUsuario";
 
-// Criando conexão
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Verificando a conexão
 if ($conn->connect_error) {
     die("Conexão falhou: " . $conn->connect_error);
 }
 
-// Verificando se o formulário foi enviado
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Coletando dados do formulário
     $email = $_POST['email'];
     $username = $_POST['username'];
     $genero = $_POST['genero'];
     $senha = $_POST['senha'];
     $confirmar_senha = $_POST['confirmar_senha'];
 
-    // Validando se as senhas coincidem
     if ($senha !== $confirmar_senha) {
         echo "As senhas não coincidem!";
         exit();
     }
 
-    // Criptografando a senha
     $senha_hash = password_hash($senha, PASSWORD_DEFAULT);
 
-    // Inserindo no banco de dados
     $sql = "INSERT INTO usuarios (email, username, genero, senha) 
             VALUES ('$email', '$username', '$genero', '$senha_hash')";
 
@@ -41,7 +33,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         echo "Erro: " . $sql . "<br>" . $conn->error;
     }
 
-    // Fechando a conexão
     $conn->close();
 }
 ?>
